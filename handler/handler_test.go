ced4de8f34cf99ee9822b723efc29d374b0ed170
heifara BUVAL <heifara.buval@gmail.com>
2021-10-25 10:36:26 +0200

    :white_check_mark: add test for computations and webhooks
    test added

---
+++ handler/handler_test.go
@@ -0,0 +1,49 @@
+package datastoreHandlers
+
+import (
+	"github.com/Heifarabuval/Cloud-Datastore-PubSub/models"
+	"github.com/stretchr/testify/mock"
+)
+
+type(
+	MockedStoreWebhook struct{
+		StoreWebhook
+		mock.Mock
+	}
+
+	MockedStoreComputation struct{
+		StoreComputation
+		mock.Mock
+	}
+)
+
+func (s *MockedStoreWebhook) Read(id int64) (*models.Webhook, error) {
+	args := s.Called(id)
+	return args.Get(0).(*models.Webhook), args.Error(1)
+}
+
+
+func (s *MockedStoreWebhook) ReadAll(id int64) ([]*models.Webhook, error) {
+	args := s.Called()
+	return args.Get(0).([]*models.Webhook), args.Error(1)
+}
+
+func CreateMockedHandler() (h Handler, mockedStoreWebhook *MockedStoreWebhook, mockedStoreComputation *MockedStoreComputation){
+	mockedStoreWebhook= &MockedStoreWebhook{}
+	mockedStoreComputation= &MockedStoreComputation{}
+	var storeWebhook StoreWebhook = mockedStoreWebhook
+	var storeComputation StoreComputation = mockedStoreComputation
+	h= Handler{
+		StoreWebhook:     storeWebhook,
+		StoreComputation: storeComputation,
+	}
+	return
+}
+
+/*func CreateEchoTest(path string, reader io.Reader, method string)(c echo.Context, req *http.Request, res *httptest.ResponseRecorder){
+	e:= echo.New()
+
+}*/
+
+
+/*func initMockHandler()(h )*/
--- handler/webhook_test.go
+++ handler/webhook_test.go
@@ -1,4 +1,4 @@
-package handler
+package datastoreHandlers
 
 import (
 	"github.com/stretchr/testify/assert"
